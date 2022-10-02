82342c5d54ca0140e2183deee77fce69d01e8a42
Ryan Punt <ryan@mirum.org>
2022-10-02 16:13:08 -0500

    clean up test files

--- f1apireader_test.go
+++ f1apireader_test.go
@@ -5,13 +5,6 @@ import (
 )
 
 func TestF1apireader(t *testing.T) {
-	// race_results :=
-	// res, err = RaceResults("")
-	// if err != nil {
-	// 	fmt.Println("err:", err)
-	// }
-	// want := "Empty URL"
-
 	msg, err := RaceResults("https://api.formula1.com/v1/event-tracker")
 	if err != nil {
 		t.Fatalf(`RaceResults("") = %q, %v, want "", error`, msg, err)
@@ -19,13 +12,6 @@ func TestF1apireader(t *testing.T) {
 }
 
 func TestF1apireaderEmpty(t *testing.T) {
-	// race_results := Race_Status("https://api.formula1.com/v1/event-tracker")
-	// res, err = RaceResults("")
-	// if err != nil {
-	// 	fmt.Println("err:", err)
-	// }
-	// want := "Empty URL"
-
 	msg, err := RaceResults("")
 	if msg != nil || err == nil {
 		t.Fatalf(`RaceResults("") = %q, %v, want "", error`, msg, err)
