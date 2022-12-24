ede0fec45242487797cdaab15897fc9036a359d0
Ryan Punt <ryan@mirum.org>
2022-12-24 10:22:10 -0600

    decompose types, reorg struct for clarity

--- f1apireader.go
+++ f1apireader.go
@@ -12,83 +12,120 @@ import (
 )
 
 type Race_Status struct {
-	RaceHubID              string
-	Locale                 string
-	CreatedAt              string
-	UpdatedAt              string
-	FomRaceID              string
-	BrandColourHexadecimal string
-	CircuitSmallImage      struct {
-		Title string
-		Path  string
-		Url   string
-	}
-	Links []struct {
-		Text string
-		Url  string
-	}
-	SeasonContext struct {
-		Id                      string
-		ContentType             string
-		CreatedAt               time.Time
-		UpdatedAt               time.Time
-		Locale                  string
-		SeasonYear              string
-		CurrentOrNextMeetingKey string
-		State                   string
-		EventState              string
-		LiveEventId             string
-		LiveTimingsSource       string
-		LiveBlog                struct {
-			ContentType     string
-			Title           string
-			ScribbleEventId string
-		}
-		SeasonState                  string
-		RaceListingOverride          int
-		DriverAndTeamListingOverride int
-		Timetables                   []struct {
-			State       string
-			Session     string
-			GmtOffset   string
-			Description string
-			EndTime     string //time.Time
-			StartTime   string //time.Time
-		}
-		ReplayBaseUrl        string
-		SeasonContextUIState int
-	}
-	RaceResults []struct {
-		DriverTLA        string
-		DriverFirstName  string
-		DriverLastName   string
-		TeamName         string
-		PositionNumber   string
-		RaceTime         string
-		TeamColourCode   string
-		GapToLeader      string
-		DriverImage      string
-		DriverNameFormat string
-	}
-	Race struct {
-		MeetingCountryName  string
-		MeetingStartDate    time.Time
-		MeetingOfficialName string // "FORMULA 1 PIRELLI GRAN PREMIO D’ITALIA 2022",
-		MeetingEndDate      time.Time
-	}
-	SeasonYearImage string
-	SessionLinkSets struct {
-		ReplayLinks []struct {
-			Session  string
-			Text     string
-			Url      string
-			LinkType string
-		}
-	}
+	RaceHubID              string            `json:"raceHubId,omitempty"`
+	Locale                 string            `json:"locale,omitempty"`
+	CreatedAt              time.Time         `json:"createdAt,omitempty"`
+	UpdatedAt              time.Time         `json:"updatedAt,omitempty"`
+	FomRaceID              string            `json:"fomRaceId,omitempty"`
+	BrandColourHexadecimal string            `json:"brandColourHexadecimal,omitempty"`
+	Headline               string            `json:"headline,omitempty"`
+	CuratedSection         CuratedSection    `json:"curatedSection,omitempty"`
+	CircuitSmallImage      CircuitSmallImage `json:"circuitSmallImage,omitempty"`
+	Links                  []Links           `json:"links,omitempty"`
+	SeasonContext          SeasonContext     `json:"seasonContext,omitempty"`
+	RaceResults            []RaceResults     `json:"raceResults,omitempty"`
+	Race                   Race              `json:"race,omitempty"`
+	SeasonYearImage        string            `json:"seasonYearImage,omitempty"`
+	SessionLinkSets        SessionLinkSets   `json:"sessionLinkSets,omitempty"`
+}
+type Image struct {
+	Title string `json:"title,omitempty"`
+	Path  string `json:"path,omitempty"`
+	URL   string `json:"url,omitempty"`
+}
+type Thumbnail struct {
+	Caption string `json:"caption,omitempty"`
+	Image   Image  `json:"image,omitempty"`
+}
+type Items struct {
+	ID              string    `json:"id,omitempty"`
+	UpdatedAt       time.Time `json:"updatedAt,omitempty"`
+	RealUpdatedAt   time.Time `json:"realUpdatedAt,omitempty"`
+	Locale          string    `json:"locale,omitempty"`
+	Title           string    `json:"title,omitempty"`
+	Slug            string    `json:"slug,omitempty"`
+	ArticleType     string    `json:"articleType,omitempty"`
+	MetaDescription string    `json:"metaDescription,omitempty"`
+	Thumbnail       Thumbnail `json:"thumbnail,omitempty"`
+	MediaIcon       string    `json:"mediaIcon,omitempty"`
+}
+type CuratedSection struct {
+	ContentType string  `json:"contentType,omitempty"`
+	Title       string  `json:"title,omitempty"`
+	Items       []Items `json:"items,omitempty"`
+}
+type CircuitSmallImage struct {
+	Title string `json:"title,omitempty"`
+	Path  string `json:"path,omitempty"`
+	URL   string `json:"url,omitempty"`
+}
+type Links struct {
+	Text string `json:"text,omitempty"`
+	URL  string `json:"url,omitempty"`
+}
+type LiveBlog struct {
+	ContentType     string `json:"contentType,omitempty"`
+	Title           string `json:"title,omitempty"`
+	ScribbleEventID string `json:"scribbleEventId,omitempty"`
+}
+type Timetables struct {
+	State       string `json:"state,omitempty"`
+	Session     string `json:"session,omitempty"`
+	GmtOffset   string `json:"gmtOffset,omitempty"`
+	Description string `json:"description,omitempty"`
+	EndTime     string `json:"endTime,omitempty"`
+	StartTime   string `json:"startTime,omitempty"`
+}
+type SeasonContext struct {
+	ID                           string       `json:"id,omitempty"`
+	ContentType                  string       `json:"contentType,omitempty"`
+	CreatedAt                    time.Time    `json:"createdAt,omitempty"`
+	UpdatedAt                    time.Time    `json:"updatedAt,omitempty"`
+	Locale                       string       `json:"locale,omitempty"`
+	SeasonYear                   string       `json:"seasonYear,omitempty"`
+	CurrentOrNextMeetingKey      string       `json:"currentOrNextMeetingKey,omitempty"`
+	State                        string       `json:"state,omitempty"`
+	EventState                   string       `json:"eventState,omitempty"`
+	LiveEventID                  string       `json:"liveEventId,omitempty"`
+	LiveTimingsSource            string       `json:"liveTimingsSource,omitempty"`
+	LiveBlog                     LiveBlog     `json:"liveBlog,omitempty"`
+	SeasonState                  string       `json:"seasonState,omitempty"`
+	RaceListingOverride          int          `json:"raceListingOverride,omitempty"`
+	DriverAndTeamListingOverride int          `json:"driverAndTeamListingOverride,omitempty"`
+	Timetables                   []Timetables `json:"timetables,omitempty"`
+	ReplayBaseURL                string       `json:"replayBaseUrl,omitempty"`
+	SeasonContextUIState         int          `json:"seasonContextUIState,omitempty"`
+}
+type RaceResults struct {
+	DriverTLA        string `json:"driverTLA,omitempty"`
+	DriverFirstName  string `json:"driverFirstName,omitempty"`
+	DriverLastName   string `json:"driverLastName,omitempty"`
+	TeamName         string `json:"teamName,omitempty"`
+	PositionNumber   string `json:"positionNumber,omitempty"`
+	RaceTime         string `json:"raceTime,omitempty"`
+	TeamColourCode   string `json:"teamColourCode,omitempty"`
+	GapToLeader      string `json:"gapToLeader,omitempty"`
+	DriverImage      string `json:"driverImage,omitempty"`
+	DriverNameFormat string `json:"driverNameFormat,omitempty"`
+}
+type Race struct {
+	MeetingCountryName  string    `json:"meetingCountryName,omitempty"`
+	MeetingStartDate    time.Time `json:"meetingStartDate,omitempty"`
+	MeetingOfficialName string    `json:"meetingOfficialName,omitempty"`
+	MeetingEndDate      time.Time `json:"meetingEndDate,omitempty"`
+}
+type ReplayLinks struct {
+	Session  string `json:"session,omitempty"`
+	Text     string `json:"text,omitempty"`
+	URL      string `json:"url,omitempty"`
+	LinkType string `json:"linkType,omitempty"`
+}
+type SessionLinkSets struct {
+	ReplayLinks []ReplayLinks `json:"replayLinks,omitempty"`
 }
 
 // Consume the F1 API for the most recent race results
-func RaceResults(URL string) (*Race_Status, error) {
+func Race_Results(URL string) (*Race_Status, error) {
 	if URL == "" {
 		return nil, errors.New("empty url")
 	}
@@ -132,16 +169,16 @@ func (r Race_Status) Status() (string, error) {
 	return "", errors.New("unable to retrieve race timetable: no \"Race\" block")
 }
 
-func (r Race_Status) Winner() (string, error) {
-	var driverTLA string
+func (r Race_Status) Winner() (RaceResults, error) {
+	var winner RaceResults
 	for _, result := range r.RaceResults {
 		position, err := strconv.Atoi(result.PositionNumber)
 		if err != nil {
-			return "", err
+			return winner, err
 		}
 		if position == 1 {
-			driverTLA = result.DriverTLA
+			winner = result
 		}
 	}
-	return driverTLA, nil
+	return winner, nil
 }
--- f1apireader_test.go
+++ f1apireader_test.go
@@ -8,29 +8,30 @@ import (
 )
 
 func TestF1apireader(t *testing.T) {
-	race, err := RaceResults("https://api.formula1.com/v1/event-tracker")
+	race, err := Race_Results("https://api.formula1.com/v1/event-tracker")
 	if err != nil {
 		t.Errorf(`RaceResults("") = %q, %v, want "", error`, race, err)
 	}
 }
 
 func TestF1apireaderEmpty(t *testing.T) {
-	msg, err := RaceResults("")
+	msg, err := Race_Results("")
 	if msg != nil || err == nil {
 		t.Fatalf(`RaceResults("") = %q, %v, want "", error`, msg, err)
 	}
 }
 
 func TestResultParsing(t *testing.T) {
-	race_test_data := ReadTestData("test_data/verstappen_wins.json")
+	race := ReadTestData("test_data/verstappen_wins.json")
 	var want string
 	want = "VER"
-	if got, _ := race_test_data.Winner(); got != want {
+	got, _ := race.Winner()
+	if got.DriverTLA != want {
 		t.Errorf(`Winner() = %q, want "%v"`, got, want)
 	}
 
 	want = "completed"
-	if got, _ := race_test_data.Status(); got != want {
+	if got, _ := race.Status(); got != want {
 		t.Errorf(`Status() = %q, want "%v"`, got, want)
 	}
 }
